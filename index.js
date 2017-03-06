974d2347f28668a40918f786963082b3758eb15f
Jan Macháček <jan.machacek@topmonks.com>
2017-03-06 09:42:32 +0100

    Code reformat & typo fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const program = require('commander');
-const convertor = require('./lib/index');
+const converter = require('./lib/index');
 
 program
-  .version('0.0.1')
-  .option('-o, --output', 'output path (default "./generated/")"')
+  .version('1.0.0')
+  .option('-o, --output', 'output path (default "./generated/")')
   .option('-i, --input [optional]', 'svg icon source path (default "./icons/")')
   .option('-f, --font [optional]', 'font config file (default "./font.json")')
   .parse(process.argv);
 
-convertor.generate({
+converter.generate({
   fontConfig: require(program.font || './font.json'),
   outputPath: program.output || './generated',
   glyphsPath: program.input || 'icons'
