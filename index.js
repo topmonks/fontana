99a96e64c4a8d6be9b936519539531e8e489b97b
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2017-03-22 11:26:08 +0100

    Generate all

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const converter = require('./lib/index');
 program
   .version('1.0.0')
   .option('-o, --output', 'output path (default "./generated/")')
+  .option('-a, --all  [optional]', 'generate font from all icon in icon folder ')
   .option('-i, --input [optional]', 'svg icon source path (default "./icons/")')
   .option('-f, --font [optional]', 'font config file (default "./font.json")')
   .parse(process.argv);
 
-converter.generate({
-  fontConfig: require(program.font || './font.json'),
-  outputPath: program.output || './generated',
-  glyphsPath: program.input || 'icons'
-});
+if (program.all) {
+  converter.generateAll();
+} else {
+  converter.generate({
+    fontConfig: require(program.font || './font.json'),
+    outputPath: program.output || './generated',
+    glyphsPath: program.input || 'icons'
+  });
+}
