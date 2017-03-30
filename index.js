e57e671f3109898b9b2733a4d335f0c0537a677e
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2017-03-30 11:23:47 +0200

    Coding standard

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const program = require('commander');
 const converter = require('./lib/index');
 
 program
-  .version('1.0.0')
-  .option('-o, --output', 'output path (default "./generated/")')
-  .option('-a, --all  [optional]', 'generate font from all icon in icon folder ')
-  .option('-i, --input [optional]', 'svg icon source path (default "./icons/")')
-  .option('-f, --font [optional]', 'font config file (default "./font.json")')
-  .parse(process.argv);
+	.version('1.0.0')
+	.option('-o, --output', 'output path (default "./generated/")')
+	.option('-a, --all  [optional]', 'generate font from all icon in icon folder ')
+	.option('-i, --input [optional]', 'svg icon source path (default "./icons/")')
+	.option('-f, --font [optional]', 'font config file (default "./font.json")')
+	.parse(process.argv);
 
 
 program.input = program.input || './icons';
 
 if (program.all) {
-  converter.generateAll();
+	converter.generateAll();
 } else {
-converter.generate({
-  fontConfig: program.all ? converter.fetchConfig(program.input) : require(program.font || './font.json'),
-  outputPath: program.output || './generated',
-  glyphsPath: program.input
-});
+	converter.generate({
+		fontConfig: program.all ? converter.fetchConfig(program.input) : require(program.font || './font.json'),
+		outputPath: program.output || './generated',
+		glyphsPath: program.input
+	});
 }
